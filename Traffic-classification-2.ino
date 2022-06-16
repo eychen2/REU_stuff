36217a050ed0f8121cbdb09603cf9478f93d3f81
eychen2 <81780994+eychen2@users.noreply.github.com>
2022-06-16 18:11:37 -0400

    Add files via upload
    Update arduino file and model byte array

--- Traffic-classification-2.ino
+++ Traffic-classification-2.ino
@@ -4,23 +4,20 @@
 #include "tensorflow/lite/micro/micro_interpreter.h"
 #include "tensorflow/lite/schema/schema_generated.h"
 #include "tensorflow/lite/micro/micro_mutable_op_resolver.h"
-#include "tensorflow/lite/version.h"
 
 #include "model-final.h"
 namespace {
   tflite::ErrorReporter* error_reporter = nullptr;
   const tflite::Model* model = nullptr;
   tflite::MicroInterpreter* interpreter = nullptr;
-  constexpr int kTensorArenaSize=1024*10;
+  constexpr int kTensorArenaSize=1024*20;
   uint8_t tensor_arena[kTensorArenaSize];
   TfLiteTensor* input = nullptr;
   TfLiteTensor* output = nullptr;
 }
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(4800);
   while (!Serial);
-  Serial.println(model->version());
-  Serial.println(TFLITE_SCHEMA_VERSION);
   static tflite::MicroErrorReporter micro_error_reporter;
   error_reporter = &micro_error_reporter;
   
@@ -34,7 +31,8 @@ void setup() {
                          model->version(), TFLITE_SCHEMA_VERSION);
     return;
   }
-  static tflite::MicroMutableOpResolver<6> resolver;
+  static tflite::MicroMutableOpResolver<7> resolver;
+  resolver.AddQuantize();
   resolver.AddConv2D();
   resolver.AddMaxPool2D();
   resolver.AddRelu();
@@ -54,13 +52,13 @@ void setup() {
    // Obtain pointers to the model's input and output tensors.
   input = interpreter->input(0);
   output = interpreter->output(0);
-  Serial.print("Number of dimensions: ");
+  /*Serial.print("Number of dimensions: ");
   Serial.println(input->dims->size);
   Serial.print("Input type: ");
   Serial.println(input->type);
   Serial.print("Number of dimensions: ");
   Serial.println(output->dims->size);
-  Serial.println(output->type);
+  Serial.println(output->type);*/
 }
 
 void loop() {
