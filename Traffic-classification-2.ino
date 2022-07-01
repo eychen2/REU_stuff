820760fcbc38ea57c7d03ea664c714bdfbd1a921
eychen2 <81780994+eychen2@users.noreply.github.com>
2022-07-01 17:05:02 -0400

    Add files via upload

--- Traffic-classification-2.ino
+++ Traffic-classification-2.ino
@@ -4,8 +4,11 @@
 #include "tensorflow/lite/micro/micro_interpreter.h"
 #include "tensorflow/lite/schema/schema_generated.h"
 #include "tensorflow/lite/micro/micro_mutable_op_resolver.h"
-
+#include "rpcWiFi.h"
 #include "model-final.h"
+#include "test1d.h"
+#include "testlabel1d.h"
+using namespace std;
 namespace {
   tflite::ErrorReporter* error_reporter = nullptr;
   const tflite::Model* model = nullptr;
@@ -15,10 +18,26 @@ namespace {
   TfLiteTensor* input = nullptr;
   TfLiteTensor* output = nullptr;
 }
-const float temp[784] = {-95,-78,-61,-44,0,2,0,4,0,0,0,0,0,0,0,0,0,0,-1,-1,0,0,0,1,0,2,-51,105,0,3,-1,30,0,0,0,70,0,0,0,70,2,26,-59,1,0,0,2,26,-59,2,0,0,8,0,69,0,0,52,-48,-37,64,0,32,6,-57,13,1,2,-88,-106,1,1,24,66,1,-69,33,97,-95,-73,-114,80,36,-51,37,-9,-128,17,90,-128,-40,-85,0,0,1,1,8,10,22,-101,90,-9,5,53,108,5,99,-20,22,-115,0,2,-51,105,0,3,-1,-29,0,0,0,70,0,0,0,70,2,26,-59,2,0,0,2,26,-59,1,0,0,8,0,69,0,0,52,-96,-124,64,0,32,6,-9,100,1,1,24,66,1,2,-88,-106,33,97,1,-69,36,-51,37,-9,-95,-73,-114,81,-128,17,96,40,-110,27,0,0,1,1,8,10,5,73,-84,-40,22,-101,90,-9,-10,108,34,-40,0,2,-51,105,0,4,0,22,0,0,0,70,0,0,0,70,2,26,-59,1,0,0,2,26,-59,2,0,0,8,0,69,0,0,52,-48,36,64,0,32,6,-57,-60,1,2,-88,-106,1,1,24,66,1,-69,33,97,-95,-73,-114,81,36,-51,37,-8,-128,16,90,-128,-112,7,0,0,1,1,8,10,22,-101,98,-77,5,73,-84,-40,-24,-66,-55,-124,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+const char* ssid = "FIU_WiFi";
+const char* password =  "";
 void setup() {
-  Serial.begin(4800);
+  int check=-1;
+  Serial.begin(9600);
   while (!Serial);
+  /*WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+  delay(100);
+  Serial.println("Connecting to WiFi..");
+  WiFi.begin(ssid);
+  while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.println("Connecting to WiFi..");
+        WiFi.begin(ssid);
+    }
+  Serial.println("Connected to the WiFi network");
+  Serial.print("IP Address: ");
+  Serial.println (WiFi.localIP()); // prints out the device's IP address */
+  //wifi_promiscuous_cb_t();
   static tflite::MicroErrorReporter micro_error_reporter;
   error_reporter = &micro_error_reporter;
   
@@ -53,23 +72,71 @@ void setup() {
    // Obtain pointers to the model's input and output tensors.
   input = interpreter->input(0);
   output = interpreter->output(0);
-  for(unsigned int i=0; i<784;++i)
-    input->data.int8[i]=temp[i]/ input->params.scale + input->params.zero_point;
-  TfLiteStatus invoke_status = interpreter->Invoke();
-  if (invoke_status != kTfLiteOk) {
-    TF_LITE_REPORT_ERROR(error_reporter, "Invoke failed");
+  /*float value=0;
+  int count=0;
+  string test;
+  for(unsigned int i=0; i<1000;++i)
+  {
+    for(unsigned int j=0; j<784;++j)
+    {
+      getline(file1, test);
+      value = stof(test);
+      Serial.println(value);                             
+      delay(100);
+      input->data.int8[j]=value/ input->params.scale + input->params.zero_point;
+      delay(100);
+    }
+    TfLiteStatus invoke_status = interpreter->Invoke();
+    if (invoke_status != kTfLiteOk) {
+      TF_LITE_REPORT_ERROR(error_reporter, "Invoke failed");
     return;
+    }
+    int8_t y_quantized = output->data.int8[0];
+    float y = (y_quantized - output->params.zero_point) * output->params.scale;
+    if(y>=0.5)
+     check=1;
+    else
+      check=0;
+     getline(file2, test);
+     label = stoi(test);
+    if(check==label)
+      ++count;
   }
-  // Obtain the quantized output from model's output tensor
-  int8_t y_quantized = output->data.int8[0];
-  // Dequantize the output from integer to floating-point
-  float y = (y_quantized - output->params.zero_point) * output->params.scale;
-  Serial.println(output->data.int8[0]);
-  Serial.println(y, 4);
+  Serial.print("Number of test data predicted correctly: ");
+  Serial.println(count);*/
+  int count=0;
+  int index=0;
+  for(unsigned int a=0; a<5;++a)
+  {
+  long startMicros = millis();
+  for(unsigned int i=0; i<40;++i)
+  {
+    for(unsigned int j=0; j<784;++j)
+    {
+      input->data.int8[j]=inputs[index+j]/ input->params.scale + input->params.zero_point;
+    }
+    index+=784;
+    TfLiteStatus invoke_status = interpreter->Invoke();
+    if (invoke_status != kTfLiteOk) {
+      TF_LITE_REPORT_ERROR(error_reporter, "Invoke failed");
+    return;
+    }
+    int8_t y_quantized = output->data.int8[0];
+    float y = (y_quantized - output->params.zero_point) * output->params.scale;
+    if(y>=0.5)
+     check=1;
+    else
+      check=0;
+    if(check==labels[i])
+      ++count;
+  }
+  long endMicros = millis();
+  Serial.println("Execution time in ms: ");
+  Serial.println(endMicros - startMicros);
+  }
   
+  Serial.print("Number of test data predicted correctly: ");
+  Serial.println(count);
 }
-
-
-void loop() {
-      
+void loop() {    
 }
